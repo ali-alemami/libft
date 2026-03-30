fa0e2fd6d642eca014ab4a94c98794d259de497a
aalemami <ali.alemame01@gmail.com>
2026-03-30 18:34:02 +0300

    added free_split and strdup_2d

--- inc/libft.h
+++ inc/libft.h
@@ -55,7 +55,8 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
+void	free_string_array(char **str);
+char	**ft_strdup_2d(const char **str);
 typedef struct s_list
 {
 	void			*content;
