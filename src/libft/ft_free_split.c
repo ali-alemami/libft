137e338c76bd3b452b628ed54b19af3df8246811
aalemami <ali.alemame01@gmail.com>
2026-04-06 17:21:22 +0300

    changed function name

--- src/libft/ft_free_split.c
+++ src/libft/ft_free_split.c
@@ -6,13 +6,13 @@
 /*   By: aalemami <aalemami@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 18:04:52 by aalemami          #+#    #+#             */
-/*   Updated: 2026/03/30 18:05:51 by aalemami         ###   ########.fr       */
+/*   Updated: 2026/04/05 21:00:03 by aalemami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	free_string_array(char **str)
+void	free_split(char **str)
 {
 	char	**tmp;
 
