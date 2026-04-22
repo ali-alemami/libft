f44772de6c6c75aa39ce8846cbf659b19c72901a
aalemami <ali.alemame01@gmail.com>
2026-04-22 16:25:33 +0300

    returns and cleans if it fails in the middle

--- src/libft/ft_strdup_2d.c
+++ src/libft/ft_strdup_2d.c
@@ -6,38 +6,41 @@
 /*   By: aalemami <aalemami@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 18:05:30 by aalemami          #+#    #+#             */
-/*   Updated: 2026/04/22 14:47:37 by aalemami         ###   ########.fr       */
+/*   Updated: 2026/04/22 15:57:45 by aalemami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	get_size(const char **str)
+static int	get_size(const char **arr)
 {
 	int	i;
 
 	i = 0;
-	while (*str++)
+	while (*arr++)
 		i++;
 	return (i);
 }
 
-char	**ft_strdup_2d(const char **str)
+char	**ft_strdup_2d(const char **arr)
 {
-	char	**new_str;
+	char	**new_arr;
 	int		i;
 
-	new_str = malloc(sizeof(char *) * (get_size(str) + 1));
-	if (!new_str)
+	new_arr = malloc(sizeof(char *) * (get_size(arr) + 1));
+	if (!new_arr)
 		return (NULL);
 	i = 0;
-	while (str[i])
+	while (arr[i])
 	{
-		new_str[i] = ft_strdup(str[i]);
-		if (!new_str[i])
-			ft_free_split(new_str);
+		new_arr[i] = ft_strdup(arr[i]);
+		if (!new_arr[i])
+		{
+			ft_free_split(new_arr);
+			return (NULL);
+		}
 		i++;
 	}
-	new_str[i] = NULL;
-	return (new_str);
+	new_arr[i] = NULL;
+	return (new_arr);
 }
