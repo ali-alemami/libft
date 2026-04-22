c57835ad977270edce9ea304ac5c730b3e918f5a
aalemami <ali.alemame01@gmail.com>
2026-04-22 13:59:45 +0300

    added ft to the functions prototype

--- src/libft/ft_free_split.c
+++ src/libft/ft_free_split.c
@@ -6,13 +6,13 @@
 /*   By: aalemami <aalemami@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 18:04:52 by aalemami          #+#    #+#             */
-/*   Updated: 2026/04/05 21:00:03 by aalemami         ###   ########.fr       */
+/*   Updated: 2026/04/22 13:58:58 by aalemami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	free_split(char **str)
+void	ft_free_split(char **str)
 {
 	char	**tmp;
 
